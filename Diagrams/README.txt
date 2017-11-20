53a414d3ee3d72078ca77451f9836312cf5baa09
Dillon <diltdicker@gmai.com>
2017-11-20 13:29:07 -0700

    Initial Project Setup

---
+++ Diagrams/README.txt
@@ -0,0 +1,4 @@
+Author: Dillon Dickerson
+
+This Directory contains the documentaion for the project as well as the 
+	schematics.
--- main.c
+++ main.c
@@ -3,7 +3,9 @@
     Class SER 456
     Semster Project
 */
-
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 1000000       // 1 MHz i think
 int main(void) {
 
     return 0;
